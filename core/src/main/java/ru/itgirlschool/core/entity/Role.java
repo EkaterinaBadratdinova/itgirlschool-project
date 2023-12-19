c81ec002be9335533991bb2b4aa3f0457e829abe
EkaterinaBadratdinova <evana@mail.ru>
2023-12-18 20:55:33 -0800

    Role and RoleType were added.

---
+++ core/src/main/java/ru/itgirlschool/core/entity/Role.java
@@ -0,0 +1,49 @@
+package ru.itgirlschool.core.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Objects;
+import java.util.Set;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@Entity
+@Table(name="roles")
+public class Role {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role_name", nullable = false)
+    private RoleType role;
+    @ManyToMany(mappedBy = "roles")
+    private Set<CustomUser> CustomUsers;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Role CustomUserRole = (Role) o;
+        if (!Objects.equals(id, CustomUserRole.id)) {
+            return false;
+        }
+        return this.role == CustomUserRole.role;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (role != null ? role.hashCode() : 0);
+        return result;
+    }
+}
