b4136d850cadafc8d46c4022cea00b8fcdc1ee0f
lerocean <vpodezva@gmail.com>
2023-12-21 22:04:29 +0300

    8: add class CustomUserResponseDto

---
+++ web1/src/main/java/ru/itgirlschool/web1/dto/CustomUserResponseDto.java
@@ -0,0 +1,45 @@
+package ru.itgirlschool.web1.dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+
+public class CustomUserResponseDto {
+
+    @NotBlank(message = "First name is required")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    private String lastName;
+
+    @NotNull(message = "Age is required")
+    @Min(value = 18, message = "Age must be at least 18")
+    private Integer age;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
+
+
