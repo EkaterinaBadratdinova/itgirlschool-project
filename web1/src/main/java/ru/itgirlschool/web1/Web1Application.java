636bbb37fff749460612263e070bcf8fb8f40bb0
EkaterinaBadratdinova <evana@mail.ru>
2023-12-25 01:04:44 -0800

    Web1: Edits in Web1Application: @EnableFeignClients was added.

--- web1/src/main/java/ru/itgirlschool/web1/Web1Application.java
+++ web1/src/main/java/ru/itgirlschool/web1/Web1Application.java
@@ -2,8 +2,10 @@ package ru.itgirlschool.web1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class Web1Application {
 
 	public static void main(String[] args) {
