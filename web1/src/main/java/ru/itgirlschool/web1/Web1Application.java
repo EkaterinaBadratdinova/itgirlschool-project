1d21c6ba1be02cd7d16f3787d0f744aa8a9e15d1
EkaterinaBadratdinova <evana@mail.ru>
2023-12-28 19:41:09 -0800

    Project: Enabled Swagger.

--- web1/src/main/java/ru/itgirlschool/web1/Web1Application.java
+++ web1/src/main/java/ru/itgirlschool/web1/Web1Application.java
@@ -1,11 +1,14 @@
 package ru.itgirlschool.web1;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@OpenAPIDefinition(info = @Info(title = "WEB1 API", version = "1.0", description = "REST API for interaction with CORE"))
 public class Web1Application {
 
 	public static void main(String[] args) {
